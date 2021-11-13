d17a5dc05c3df585fa6fa87e0f1435bfb74c057d
Adam K <krueng@gmail.com>
2021-11-13 17:27:04 -0600

    initiated tables

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,44 @@
 exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.integer('phone', 200).notNullable()
+      users.increments('user_id');
+      users.string('username', 200).notNullable();
+      users.string('password', 200).notNullable();
+      users.integer('phone', 200).notNullable();
+    })
+    .createTable('plants', (plants) => {
+      plants.increments('plant_id');
+      plants.string('nickname', 200).notNullable();
+      plants.string('species', 200).notNullable();
+      plants.string('image_url', 200);
+      plants.integer('user_id', 200).notNullable()
+      .references('user_id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    })
+    .createTable('schedules', sch => {
+      sch.increments('schedule_id');
+      sch.integer('plant_id')
+      .notNullable()
+      .references('plant_id')
+      .inTable('plants')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+      sch.integer('user_id')
+      .notNullable()
+      .references('user_id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+      sch.string('water_frquency', 200).notNullable();
+      
     })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema
+    .dropTableIfExists('schedules')
+    .dropTableIfExists('plants')
+    .dropTableIfExists('users');
 }
